593ad1e069bb3fb062f28075fc03421f150d2cea
sobrock12 <59989171+sobrock12@users.noreply.github.com>
2020-04-01 00:27:40 -0500

    Update database.php

--- model/database.php
+++ model/database.php
@@ -1,9 +1,10 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
+    $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=u26sv8lv0m5wz55b';
+    $username = 'iqahukqx0bz5wzz6';
+    $password = 'x5v8ho7v0b6fdled';
 
     try {
-        $db = new PDO($dsn, $username);
+        $db = new PDO($dsn, $username, $password);
     } catch (PDOException $e) {
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
