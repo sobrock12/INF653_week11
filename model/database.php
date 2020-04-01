b7b475907297d14b1341d26899452c0613b5b72b
Stanley Obrock <sobrock12@gmail.com>
2020-04-01 00:37:11 -0500

    third commit, syntax error fix in database.php

--- model/database.php
+++ model/database.php
@@ -1,6 +1,6 @@
 <?php
-//heroku deployment
-/*
+    //heroku deployment
+    /*
     $dsn = 'mysql:host=zy4wtsaw3sjejnud.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=u26sv8lv0m5wz55b';
     $username = 'iqahukqx0bz5wzz6';
     $password = 'x5v8ho7v0b6fdled';
@@ -12,9 +12,9 @@
         include('../errors/database_error.php');
         exit();
     }
-*/
+    */
 
-//localhost testing
+    //localhost testing
     $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
     $username = 'root';
 
@@ -24,4 +24,5 @@
         $error_message = $e->getMessage();
         include('../errors/database_error.php');
         exit();
+    }
 ?>
